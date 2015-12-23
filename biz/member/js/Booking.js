99a1721de4720a211e4820d764f040086565b2f6
ronght <anyuelang@gmail.com>
2015-12-23 22:12:09 +0800

    会员预订页面

--- biz/member/js/Booking.js
+++ biz/member/js/Booking.js
@@ -2,21 +2,46 @@
  * Created by Maxim on 2015/12/19.
  */
 $(function () {
-    $(function () {
-        $('.slick-container').slick({
-            infinite: false,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });
 
-//        $(".booking-block").addClass("block-fadeUp").show();
+    $(".staff-choose > li").click(function() {
+        var $this = $(this);
+        $this.siblings().removeClass("selected").addClass("unselected");
+        $this.removeClass("unselected").addClass("selected");
+        var staffId = $this.data("id");
+        $.ajax({
+            url: context + "/member/booking/show/" + staffId,
+            success : function (data) {
+                var match = data.match(/<!--\s*replace-start\s*-->([\s\S]+)<!--\s*replace-end\s*-->/)[1];
+                $(".booking-panel").replaceWith(match);
+            }
+        }).done(function() {
+        });
     });
 
-    $(".date-circle > .booking-date").click(function () {
-        $(".date-circle > .booking-date").removeClass("selected");
-        var day = $(this).data("day");
-        $(this).addClass("selected")
-        $(".day-list-fake").hide();
-        $("#" + day).show();
+    $(".main").click(function(e) {
+        var $target = $(e.target);
+        if($target.attr("class") == "booking-block" || $target.closest('.booking-block').length > 0) {
+            return
+        }
+        if($target.attr("class") == "booking-time" || $target.closest('.booking-time').length > 0) {
+            return
+        }
+        $("#booking-block").hide();
+    })
+
+    $(".booking-btn").click(function() {
+        var data = {
+            "staffId" : $("#comfirm-booking-staff").data("staffId"),
+            "millis" : $("#comfirm-booking-time").data("millis")
+        };
+         $.ajax({
+             url: context + "/member/booking/booking/",
+             type: "POST",
+             data: data,
+             dataType: "json",
+             success : function (data) {
+                 alert("预订成功");
+             }
+         });
     });
 });
---
+++ biz/member/js/Subscribe.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Maxim on 2015/12/23.
+ */
+$(function () {
+
+});
